320ea488e5651efb0af85a37e41c81b7a54f7f4d
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-19 16:59:22 -0600

    Completed the Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let build tinder clone app</h1>
+      {/* Header */}
+      <Header/>
+      {/* Tinder Cards */}
+      {/* Button below tinder card */}
+
+      {/* Chat Screens */}
+      {/* Individual chat screen */}
     </div>
+    
   );
 }
 
