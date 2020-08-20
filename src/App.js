91faf345c1a5c3a3ed97407616c21909cdc37069
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-20 11:57:57 -0600

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
-      <Header/>
-      {/* Tinder Cards */}
-      {/* Button below tinder card */}
 
-      {/* Chat Screens */}
-      {/* Individual chat screen */}
+      
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am the chat page</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am home</h1>
+          </Route>
+        </Switch>
+
+      </Router>
+
     </div>
     
-  );
-}
+    );
+  }
+  
+  export default App;
+  
+  {/* Tinder Cards */}
+  {/* Button below tinder card */}
 
-export default App;
+  {/* Chat Screens */}
+  {/* Individual chat screen */}
