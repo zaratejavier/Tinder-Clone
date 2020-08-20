69f69527217027d441a8e52c373422b3f3f76c15
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-20 16:24:51 -0600

    images and names working from firebase

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TinderCard from "react-tinder-card"
 import "./TinderCards.css"
+import database from './firebase'
 
 const TinderCards = () => {
-  const [people, setpeople] = useState([
-    {
-      name: "Steve Jobs",
-      url: "https://www.gstatic.com/tv/thumb/persons/476283/476283_v9_ba.jpg"
-    },
-    {
-      name: "Bill Gates",
-      url: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iHiqcJBsYq6c/v0/640x-1.jpg"
-    }
-  ])
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    //we got to firebase to the people 'collection' and listen to whenever anything changes inside that collection
+    //onSnapshot - its basically like taking a picture of the database, and it is going to give you back all the documents inside
+    //snapshot - we now have that with all the documents inside of it
+    //snapshot.doc.map - will get you what is inside the doc and we map through them
+    // doc.data - will have each individual person
+    // setPeople - we will add all of the data we got to the state
+    database
+      .collection("people")
+      .onSnapshot((snapshot) =>
+        setPeople(snapshot.docs.map((doc) => doc.data()))
+      );
+  },[])
+
   return (
     <div>
       <h1>Tinder Cards</h1>
