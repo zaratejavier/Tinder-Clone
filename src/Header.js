cdde88b4acb8851be397dc8700b37894f9958ada
Javier Zarate <zarate.javier.jz@gmail.com>
2020-08-25 19:10:12 -0600

    if we are in that page we will see a back arrow else just the person icon

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,31 @@ import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import IconButton from "@material-ui/core/IconButton"
-import { Link } from 'react-router-dom';
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos"
+import {Link, useHistory} from "react-router-dom"
 
-const Header = () => {
+
+const Header = ({ backButton }) => {
+  const history = useHistory(); //gives us the browser history
+
   return (
     <div className="header">
-      <IconButton> 
-        <PersonIcon className="header__icon" fontSize="large" />
-      </IconButton>  
-      <img
-        className="header__logo"
-        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
-        alt="Tinder Logo" />
+      {/* if there is a backButton prop render the ArrowBackButton, else render the personIcon */}
+      {backButton ? (
+        <IconButton onClick={() => history.replace(backButton)}>
+          <ArrowBackIosIcon fontSize="large" className="header_icon"/>
+        </IconButton>
+      ) : (
+        <IconButton> 
+          <PersonIcon className="header__icon" fontSize="large" />
+        </IconButton>  
+      )}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+          alt="Tinder Logo" />
+      </Link>
       <Link to="/chat">
         <IconButton>
           <ForumIcon className="header__icon" fontSize="large" />
